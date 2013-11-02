fb6361753f83939be99cba0411987149b6924a3d
Prashant <prashant.barca@gmail.com>
2013-11-02 15:42:59 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaughingAdventure::Application.config.secret_key_base = '763d2e8c4624d83d8e6f0748f72bc68465e9f4a757004d0cd7b1df1cd6c46dbe45f213f26f285993ccdaf2189a535b1bfa3e9280fc4832d3dba4348a32f77a37'
