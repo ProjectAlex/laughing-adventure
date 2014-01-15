4b5f2a46bc8af93ad4cfb99afe5ad05514bf7ca1
Mithul <mithulmathi@gmail.com>
2014-01-15 21:24:45 +0530

    comment profile pic added. change database.yml again

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:body))
 		@comment.commenter= current_user.name
+		@comment.user = current_user
 		respond_to do |format|
       if @comment.save
         format.html { redirect_to root_path }
