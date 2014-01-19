09e078d3c98061971d949e33e21549a65e446ac0
Mithul <mithulmathi@gmail.com>
2014-01-19 16:19:45 +0530

    limited to one like or dislike. there are bugs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,17 +5,33 @@ class HomeController < ApplicationController
   end
 
  def votedup
- @post = Post.find(params[:id])
- @post.ups=@post.ups+1
- @post.save
- render :text => "<div class='up'></div>"+@post.ups.to_s+" likes"
+  @post = Post.find(params[:id])
+  @likercheck=@post.likers.where(:id == current_user.id).first
+  if(@likercheck.nil?)
+    @post.ups=@post.ups+1
+    @post.likers << current_user
+  end
+  @dislikercheck=@post.dislikers.where(:id == current_user.id).first
+  if (!@dislikercheck.nil?)
+    @post.downs=@post.downs-1
+  end
+  @post.save
+  render :text => "<div class='up'></div>"+@post.ups.to_s+" likes"
  end
 
  def voteddown
- @post = Post.find(params[:id])
- @post.downs=@post.downs+1
- @post.save
- render :text => "<div class='down'></div>"+@post.downs.to_s+" dislikes"
+  @post = Post.find(params[:id])
+  @dislikercheck=@post.dislikers.where(:id == current_user.id).first
+  if(@dislikercheck.nil?)
+    @post.downs=@post.downs+1
+    @post.dislikers << current_user
+  end
+  @likercheck=@post.likers.where(:id == current_user.id).first
+  if (!@likercheck.nil?)
+    @post.ups=@post.ups-1
+  end
+  @post.save
+  render :text => "<div class='up'></div>"+@post.ups.to_s+" dislikes"
  end
 
 
