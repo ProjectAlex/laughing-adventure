95737b7c14241fa2e91baed7458cd67b2fb1855b
Mithul <mithulmathi@gmail.com>
2014-01-19 16:19:45 +0530

    limited to one like or dislike. there are bugs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117065721) do
+ActiveRecord::Schema.define(version: 20140119101740) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20140117065721) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "dislikers", id: false, force: true do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+  end
+
+  add_index "dislikers", ["user_id", "post_id"], name: "index_dislikers_on_user_id_and_post_id"
 
   create_table "follows", force: true do |t|
     t.integer  "followable_id",                   null: false
@@ -50,6 +57,13 @@ ActiveRecord::Schema.define(version: 20140117065721) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "likers", id: false, force: true do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+  end
+
+  add_index "likers", ["user_id", "post_id"], name: "index_likers_on_user_id_and_post_id"
 
   create_table "messages", force: true do |t|
     t.string   "sender_id",                           null: false
