6c39ca9490bdf7002a4446a6e1138f706b65220c
Mithul <mithulmathi@gmail.com>
2014-01-26 18:16:57 +0530

    forgot

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,12 +40,13 @@ class PostsController < ApplicationController
             if @post.save
                 ocr_link=('~/laughing-adventure/public'+@post.att_file.url).split('?')
                 #Here the adding of tags starts
+		x=[]
                 y=[]
                 z=[]
                 begin
                     a=e.text_for(ocr_link[0]).strip
                     x = word_frequencies(a+" "+@post.caption,5)
-                    y= @post.content.split(' ')
+                    y= @poset.content.split(' ')
                     #z= @post.caption.split(' ')
                 rescue
                     puts "------------------------"
