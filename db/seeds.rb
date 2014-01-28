a7785fe9d4b2a1bfa011c07e0eb2fa366f4162d2
Mithul <mithulmathi@gmail.com>
2014-01-28 21:50:01 +0530

    error was with rolify gem https://github.com/EppO/rolify/commit/26ec434ef6d1a544f9a76b560ea0f4efae006600 is fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,9 @@ YAML.load(ENV['ROLES']).each do |role|
   Role.find_or_create_by_name(role)
   puts 'role: ' << role
 end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
-user.confirm!
-user.add_role :admin
+puts 'First user registered is admin'
+# puts 'DEFAULT USERS'
+# user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+# puts 'user: ' << user.name
+# user.confirm!
+# user.add_role :admin
