08e16e4fc7d2591250376e2529bc688dfdd6c919
Mithul <mithulmathi@gmail.com>
2014-02-06 23:43:13 +0530

    pdfs and docs allowed. post only 1 like and dislike fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,15 +6,15 @@ class HomeController < ApplicationController
 
  def votedup
   @post = Post.find(params[:id])
-  @likercheck=@post.likers.where(:id == current_user.id).first
-  if(@likercheck.nil?)
+  @likercheck=@post.likers.where(id: current_user.id).first
+  if(@likercheck.nil? || @likercheck.id!=current_user.id)
     @post.ups=@post.ups+1
     @post.likers << current_user
-  end
   @dislikercheck=@post.dislikers.where(:id == current_user.id).first
   if (!@dislikercheck.nil?)
     @post.downs=@post.downs-1
     @post.dislikers.delete(current_user)
+  end
   end
   @post.save
   render :text => "<div class='up'></div>"+@post.ups.to_s+" likes <<<<<<<< <div class='down'></div>"+@post.downs.to_s+" dislikes"
@@ -22,15 +22,15 @@ class HomeController < ApplicationController
 
  def voteddown
   @post = Post.find(params[:id])
-  @dislikercheck=@post.dislikers.where(:id == current_user.id).first
+  @dislikercheck=@post.dislikers.where(id:  current_user.id).first
   if(@dislikercheck.nil?)
     @post.downs=@post.downs+1
     @post.dislikers << current_user
-  end
-  @likercheck=@post.likers.where(:id == current_user.id).first
+    @likercheck=@post.likers.where(:id == current_user.id).first
   if (!@likercheck.nil?)
     @post.ups=@post.ups-1
     @post.likers.delete(current_user)
+  end
   end
   @post.save
   render :text => "<div class='up'></div>"+@post.ups.to_s+" likes <<<<<<<< <div class='down'></div>"+@post.downs.to_s+" dislikes"
