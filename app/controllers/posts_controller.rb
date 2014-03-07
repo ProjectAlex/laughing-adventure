aa294524aaf6747d57228ab1dd78757209de0104
Mithul <mithulmathi@gmail.com>
2014-03-07 23:07:41 +0530

    error fixes in user n post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
 
         respond_to do |format|
             format.html # show.html.erb
-            format.json {render json: @pos t}
+            format.json {render json: @post}
         end
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,3 +56,4 @@ class UsersController < ApplicationController
             end
         end
     end
+end
