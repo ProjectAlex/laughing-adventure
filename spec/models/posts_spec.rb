a90071477420d70180fdca477b09c786ae47b19a
Prashant <prashant.barca@gmail.com>
2014-03-19 00:28:13 +0530

    Tried more specs

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
-describe Posts do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Post do
+    it "Tests for posts" do
+        prashant = Post.create!(:content => "abcd",:caption => "defg")
+        mithul = Post.create!(:content => "bcde",:caption => "efgh")
+        expect(Post.order_by_content).to eq([prashant,mithul])
+    end
 end
