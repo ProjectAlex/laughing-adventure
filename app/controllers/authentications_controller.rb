9e035bf15a72912bb598fa8fb84eb091bc0b44e5
Mithul <mithulmathi@gmail.com>
2014-04-12 14:57:35 +0530

    added animation to like and dislike

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -29,7 +29,7 @@ class AuthenticationsController < ApplicationController
   omniauth = request.env["omniauth.auth"]
   authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
   if authentication
-    flash[:notice] = "Signed in successfully."
+    flash[:notice] = "Signed in successfully using existing authentication."
     sign_in_and_redirect(:user, authentication.user)
   elsif current_user
     current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
